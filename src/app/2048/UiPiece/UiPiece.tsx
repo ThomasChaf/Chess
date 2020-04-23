58559fe20806d1a6a2533c0b4aba326d88a2a6a4
Thomas Chafiol <thomaschaf@gmail.com>
2020-04-24 01:11:42 +0200

    Add 2048 animations.

--- src/app/2048/UiPiece/UiPiece.tsx
+++ src/app/2048/UiPiece/UiPiece.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { useSpring, animated } from "react-spring";
 import cn from "classnames";
 import "./UiPiece.scss";
 
@@ -12,9 +13,35 @@ export const UiPiece = (props: PieceProps) => {
   const x = `x-${props.col}`;
   const y = `y-${props.row}`;
 
+  const ref = useRef(false);
+  const [springs, set] = useSpring(() => ({
+    from: { opacity: 0, x: 0 },
+    opacity: 1
+  }));
+  useEffect(() => {
+    set({ x: ref.current ? 1 : 0, delay: 200 });
+    ref.current = !ref.current;
+  }, [props.value, set]);
+
+  const animatedStyle = {
+    opacity: springs.opacity
+      .interpolate({
+        range: [0, 0.5, 1],
+        output: [0, 0, 1]
+      })
+      .interpolate((o: number) => o),
+    transform: springs.x
+      .interpolate({
+        range: [0, 0.9, 1],
+        output: [1, 1.1, 1]
+      })
+      .interpolate((s: number) => `scale(${s})`)
+  };
   return (
     <div className={cn("big-piece", `val-${props.value}`, x, y)}>
-      <div className="big-piece-content">{props.value}</div>
+      <animated.div style={animatedStyle} className="big-piece-content">
+        {props.value}
+      </animated.div>
     </div>
   );
 };
